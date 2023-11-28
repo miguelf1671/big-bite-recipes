4cdc63df23f692e36ea0903cb8ec2985d40695c5
Morgan Deason <mdeason8.27@gmail.com>
2023-11-28 14:16:06 -0500

    Commit before class

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
 
@@ -15,10 +16,45 @@ class Recipe(db.Model, SerializerMixin):
     who_submitted = db.Column(db.String)
     who_favorited = db.Column(db.String)
 
+    # Create a relationship between recipe and favorite
+    favorites = db.relationship("Favorite", back_populates="recipe")
+
+    # Creates an association proxy from the recipe-favorite relationship
+    users = association_proxy("favorites", "user")
+
+    # Creates serialization rules to avoid cascading
+    serialize_rules = ("-favorites.recipe")
+
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
-    favorites = db.Column(db.String, nullable=False)
-    submissions = db.Column(db.String, nullable=False)
+    user_favorites = db.Column(db.String, nullable=False)
+    user_submissions = db.Column(db.String, nullable=False)
+
+    # Create a relationship between user and favorite
+    favorites = db.relationship("Favorite", back_populates="user")
+
+    # Create an association proxy from the user-favorite relationship
+    recipes = association_proxy("favorites","recipe")
+
+    # Create serialization rules to avoid cascading
+    serialize_rules = ("-favorites.user")
+
+
+class Favorite(db.Model, SerializerMixin):
+    __tablename__ = 'favorites'
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # Relationship between favorite row and recipe row
+    recipe = db.relationship("Recipe", back_populates="favorites")
+
+    # Relationship between favorite row and user row
+    user = db.relationship("User", back_populates="favorites")
+
+    # Create serialization rules to avoid cascading
+    serialize_rules = ("-recipe.favorites", "-user.favorites")
--- server/seed.py
+++ server/seed.py
@@ -1,10 +1,12 @@
 from app import app
-from models import User, Recipe, db
+from models import User, Recipe, Favorite, db
 from faker import Faker
 from random import choice
 
 fake = Faker()
 
+
+# Create 10 fake recipes
 def create_recipes():
     recipes = []
     for _ in range(10):
@@ -19,27 +21,44 @@ def create_recipes():
         ))
     return recipes
 
+# Creates 5 fake users
+
 
 def create_users():
     users = []
     for _ in range(5):
         users.append(User(
             username=fake.name(),
-            favorites="Hopefully a future list",
-            submissions="Hopefully a future list",
+            user_favorites=fake.word(),
+            user_submissions=fake.word(),
         ))
     return users
 
+#########################################################
+### Populate table within application context manager ###
+#########################################################
+
 
 with app.app_context():
+    print('Populating database...')
+
+    print('\n\t>>> Deleting existing database tables...')
     Recipe.query.delete()
     User.query.delete()
+    Favorite.query.delete()
     db.session.commit()
+    print('\t>>> Tables deleted.')
 
+    print('\n\t>>> Generating sample data for recipes...')
     recipes = create_recipes()
     db.session.add_all(recipes)
     db.session.commit()
+    print('\t>>> Recipe data generation successful.')
 
+    print('\n\t>>> Generating sample data for users...')
     users = create_users()
     db.session.add_all(users)
     db.session.commit()
+    print('\t>>> User data generation successful.')
+
+    print('\n>>> Database populated successfully.')
